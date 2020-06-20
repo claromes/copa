f6fc030b887942fb464f77bab07fbe3aebfc0798
Claromes <claromes@protonmail.ch>
2020-06-20 02:49:37 -0300

    Fix var names

--- ccoa.sh
+++ ccoa.sh
@@ -2,34 +2,34 @@
 # FILENAME=$1
 # ALIAS=$2
 
-green=$(tput setaf 2)
-yellow=$(tput setaf 3)
-cyan=$(tput setaf 6)
-reset=$(tput sgr0)
-bold=$(tput bold)
-normal=$(tput sgr0)
+GREEN=$(tput setaf 2)
+YELLOW=$(tput setaf 3)
+CYAN=$(tput setaf 6)
+RESET=$(tput sgr0)
+BOLD=$(tput bold)
+NORMAL=$(tput sgr0)
 
-ls=$(ls -l $1)
+LIST=$(ls -l $1)
 
 sudo chmod +x $1 &&
 sudo chown -R $USER:$USER $1 &&
-echo "${cyan}info${normal} Permissions:${normal} ${ls}" &&
+echo "${CYAN}info${NORMAL} Permissions:${NORMAL} ${LIST}" &&
 sudo mkdir /opt/$2 &&
 sudo cp $1 /opt/$2 &&
-echo "${cyan}info${normal} The files was copied to /opt/$2 directory" &&
+echo "${CYAN}info${NORMAL} The files was copied to /opt/$2 directory" &&
 echo "" >> ~/.bashrc &&
 echo "# $1 - created by ccoa.sh script" >> ~/.bashrc &&
 echo "alias $2='cd /opt/$2 && ./$1 > /dev/null 2>&1 &'" >> ~/.bashrc &&
 
 while true; do
-    read -p "${bold}- Delete original files?(y/n)${reset}" yn
+    read -p "${BOLD}- Delete original files?(y/n)${RESET}" yn
     case $yn in
         [Yy]* ) sudo rm -rf $1; break;;
         [Nn]* ) break;;
     esac
 done &&
 
-echo "${yellow}warning${normal} The alias \"cd /opt/$2 && ./$1 > /dev/null 2>&1 &\" was created in your .bashrc file" &&
-echo "${green}success${normal} Type ${bold}$2${normal} to open" &&
+echo "${YELLOW}warning${NORMAL} The alias \"cd /opt/$2 && ./$1 > /dev/null 2>&1 &\" was created in your .bashrc file" &&
+echo "${GREEN}success${NORMAL} Type ${BOLD}$2${NORMAL} to open" &&
 
 exec bash
