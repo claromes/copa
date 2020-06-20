409ec7da4a41bc79fdbef582f78cf053c56ac74f
Claromes <claromes@protonmail.ch>
2020-06-20 03:33:22 -0300

    Add flags

--- ccoa.sh
+++ ccoa.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-# FILENAME=$1
-# ALIAS=$2
 
 GREEN=$(tput setaf 2)
 YELLOW=$(tput setaf 3)
@@ -11,15 +9,57 @@ NORMAL=$(tput sgr0)
 
 LIST=$(ls -l $1)
 
+VERSION="v1.0"
+CREDITS="
+    Created by Claromes
+    claromes@protonmail.ch
+    ${BOLD}https://twitter.com/claromes${NORMAL}
+
+    Under the MIT License
+    Source code: ${BOLD}https://gitlab.com/claromes/chmod-chown-opt-alias
+"
+HELP="
+    Usage: ${BOLD}./ccoa.sh [file] [alias]${NORMAL}
+
+    file:               File to change permissions and to set an alias
+    alias:              Unique word without spaces. Choose a short word
+
+
+
+    Info: ${BOLD}./ccoa.sh [info]${NORMAL}
+
+    Info options:
+
+        -h --help       Print option
+        -v --version    Print CCOA version
+        -c --credits    Print the maintainer name, contact, source code link and license
+"
+
+case "$1" in
+    -v | --version)
+                echo "$VERSION"
+                exit
+    ;;
+    -h | --help)
+                echo "$HELP"
+                exit
+    ;;
+    -c | --credits)
+                echo "$CREDITS"
+                exit
+    ;;
+esac
+
+
 sudo chmod +x $1 &&
 sudo chown -R $USER:$USER $1 &&
 echo "${CYAN}info${NORMAL} Permissions:${NORMAL} ${LIST}" &&
-sudo mkdir /opt/$2 &&
-sudo cp $1 /opt/$2 &&
-echo "${CYAN}info${NORMAL} The files was copied to /opt/$2 directory" &&
-echo "" >> ~/.bashrc &&
-echo "# $1 - created by ccoa.sh script" >> ~/.bashrc &&
-echo "alias $2='cd /opt/$2 && ./$1 > /dev/null 2>&1 &'" >> ~/.bashrc &&
+#sudo mkdir /opt/$2 &&
+#sudo cp $1 /opt/$2 &&
+#echo "${CYAN}info${NORMAL} The files was copied to /opt/$2 directory" &&
+#echo "" >> ~/.bashrc &&
+#echo "# $1 - created by ccoa.sh script" >> ~/.bashrc &&
+#echo "alias $2='cd /opt/$2 && ./$1 > /dev/null 2>&1 &'" >> ~/.bashrc &&
 
 while true; do
     read -p "${BOLD}- Delete original files?(y/n)${RESET}" yn
